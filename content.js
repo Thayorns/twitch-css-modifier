6cd1c9589e336afa1b9b6a200a27fe20582b049e
Valentine <valentine@pop-os.Dlink>
2025-06-10 20:03:00 +0300

    new class added

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 function applyStyles() {
-    const overlayElements = document.querySelectorAll('.eSKEhN, .player-overlay-background--darkness-3');
+    const overlayElements = document.querySelectorAll('.bmlSdB, .player-overlay-background--darkness-3');
     overlayElements.forEach(el => {
-      if (el.classList.contains('eSKEhN')) {
+      if (el.classList.contains('bmlSdB')) {
         el.style.display = 'none';
       } else if (el.classList.contains('player-overlay-background--darkness-3')) {
         el.style.background = 'none';
