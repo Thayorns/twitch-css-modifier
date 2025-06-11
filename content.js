0b3381b59a4d7f07619ba5c71bcf95e3390439ab
Valentine <valentine@pop-os.Dlink>
2025-06-12 02:13:35 +0300

    new class added

--- content.js
+++ content.js
@@ -2,12 +2,12 @@ function applyStyles() {
     const overlayElements = document.querySelectorAll('.bmlSdB, .player-overlay-background--darkness-3');
     overlayElements.forEach(el => {
       if (el.classList.contains('bmlSdB')) {
-        el.style.display = 'none !important';
+        el.style.setProperty('display', 'none', 'important');
       } else if (el.classList.contains('player-overlay-background--darkness-3')) {
-        el.style.background = 'none !important';
+        el.style.setProperty('background', 'none', 'important');
       }
     });
-  }
+}
   applyStyles();
   
   const observer = new MutationObserver(applyStyles);
